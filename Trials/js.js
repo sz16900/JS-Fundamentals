c6e7236ce4af4b680c3fbaa7b484a3adc982dd3d
sz16900 <sz16900@my.bristol.ac.uk>
2019-10-10 08:50:59 -0500

    first commit

---
+++ Trials/js.js
@@ -0,0 +1,23 @@
+function add7(a) {
+    return a + 7;
+};
+
+console.log(add7(1));
+
+function multiply(a, b) {
+    return a * b;
+};
+
+console.log(multiply(4, 4));
+
+function capitalize(string) {
+    return string[0].toUpperCase();
+};
+
+console.log(capitalize("seth"));
+
+function lastLetter(string) {
+    return string[string.length - 1];
+};
+
+console.log(lastLetter("seth"));
